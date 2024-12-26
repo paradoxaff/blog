78f8582cad6dc94b1f70b04caa9146465bbdf2e8
paradoxaff <waheedaffan25@gmail.com>
2024-12-26 16:19:21 +0500

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,12 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans =({
+const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
-const geistMono =({
+const geistMono =localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
